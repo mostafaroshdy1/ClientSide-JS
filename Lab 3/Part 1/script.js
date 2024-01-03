537ad63a361ea6964bd5bf83485375eaf2b63948
Mostafa Roshdy <mostafaroshdy1@gmail.com>
2024-01-03 17:52:13 +0200

    Add lab 3 Part 1 bonus

--- Lab 3/Part 1/script.js
+++ Lab 3/Part 1/script.js
@@ -8,7 +8,7 @@ while (!exit) {
             break;
 
         case 2:
-            alert('Result: ' + calc());
+            calc();
             exit = 1;
             break;
 
@@ -42,21 +42,35 @@ function logIn() {
     }
 }
 function calc() {
-    const firstNumber = parseFloat(prompt("Enter first number"));
-    const operation = prompt("enter an operation (sum + ,multi * ,subtract - ,division / ,moduls %)");
-    const secondNumber = parseFloat(prompt("Enter second number"));
-    switch (operation) {
-        case '+':
-            return firstNumber + secondNumber;
-        case '-':
-            return firstNumber - secondNumber;
-        case '*':
-            return firstNumber * secondNumber;
-        case '/':
-            return firstNumber / secondNumber;
-        case '%':
-            return firstNumber % secondNumber;
-        default:
-            return 'Invalid operation';
+    let firstNumber = parseFloat(prompt("Enter first number"));
+    let operation = 1;
+    while (true) {
+        operation = prompt("enter an operation (sum + ,multi * ,subtract - ,division / ,moduls %) Cancel to exit");
+        if (operation === null) break;
+        const secondNumber = parseFloat(prompt("Enter second number"));
+        switch (operation) {
+            case '+':
+                alert('Result: ' + (firstNumber + secondNumber));
+                firstNumber = firstNumber + secondNumber // first number is represention the sum
+                break;
+            case '-':
+                alert('Result: ' + (firstNumber - secondNumber));
+                firstNumber = firstNumber - secondNumber
+                break;
+            case '*':
+                alert('Result: ' + (firstNumber * secondNumber));
+                firstNumber = firstNumber * secondNumber
+                break;
+            case '/':
+                alert('Result: ' + (firstNumber / secondNumber));
+                firstNumber = firstNumber / secondNumber
+                break;
+            case '%':
+                alert('Result: ' + (firstNumber % secondNumber));
+                firstNumber = firstNumber % secondNumber
+                break;
+            default:
+                return 'Invalid operation';
+        }
     }
 }
