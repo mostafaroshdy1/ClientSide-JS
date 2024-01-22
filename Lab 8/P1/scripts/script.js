d5872fe2d46c84130d865472bef59d528326594e
Mostafa Roshdy <mostafaroshdy1@gmail.com>
2024-01-22 21:45:23 +0200

    Finish Lab 8 P1

--- Lab 8/P1/scripts/script.js
+++ Lab 8/P1/scripts/script.js
@@ -5,7 +5,6 @@ const leftBtn = document.querySelector(".leftBtn");
 let carouselArray = Array.from(carouselEl);
 const firstElement = carouselEl[0];
 const lastElemen = carouselEl[carouselEl.length - 1];
-console.log(carouselEl[0].classList.contains("carousel-el"));
 carouselSetup();
 
 
@@ -23,41 +22,10 @@ leftBtn.addEventListener('click', (e) => {
 
 
 function carouselSetup() {
-    // for (let i = 0; i < carouselArray.length; i++) {
-    //     if (i === carouselArray.length / 2 - 1 || i === carouselArray.length / 2 || i === carouselArray.length / 2 + 1) {
-    //         carouselArray[i].classList.remove("hidden");
-    //         if (i === carouselArray.length / 2) {
-    //             carouselArray[i].classList.add("infocus");
-    //         } else {
-    //             carouselArray[i].classList.remove("infocus");
-    //         }
-    //     } else {
-    //         carouselArray[i].classList.remove("infocus");
-    //         carouselArray[i].classList.add("hidden");
-    //     }
-    // }
-
-
     for (let i = 0; i < carouselArray.length; i++) {
         carouselArray[i].classList.add("hidden");
         carouselArray[i].classList.remove("infocus");
     }
-
-    if (carouselArray[carouselArray.length / 2].classList.contains("last")) {
-        carouselArray[carouselArray.length / 2].classList.remove("last");
-        const temp = carouselEl[0];
-        temp.classList.add("last")
-        carouselEl[0].remove();
-        carousel[0].appendChild(temp);
-        // console.log(carouselEl[0]);
-        // carouselArray = Array.from(carouselEl);
-        console.log(carouselArray);
-
-
-    }
-
-    carouselArray[carouselArray.length / 2 - 1].classList.remove("hidden");
-    carouselArray[carouselArray.length / 2 + 1].classList.remove("hidden");
     carouselArray[carouselArray.length / 2].classList.remove("hidden");
     carouselArray[carouselArray.length / 2].classList.add("infocus");
 
