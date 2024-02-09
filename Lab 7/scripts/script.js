29ce341db33fbed27596eda0857cef01d61cb2e1
Mostafa Roshdy <mostafaroshdy1@gmail.com>
2024-02-09 23:22:09 +0200

    Add local Storage Support for Todo list

--- Lab 7/scripts/script.js
+++ Lab 7/scripts/script.js
@@ -1,5 +1,4 @@
 let user;
-let tick = 0, cross = 0, taskId = 0;
 const loginBtn = document.getElementById("login");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
@@ -12,19 +11,33 @@ const body = document.getElementsByTagName("body");
 const response = document.getElementById("response");
 const a = document.getElementsByTagName('a');
 
+if (JSON.parse(localStorage.getItem("tasks")).length > 0) {
+    var tasks = JSON.parse(localStorage.getItem("tasks"));
+    var tick = tasks[tasks.length - 1].id + 1;
+    var cross = tasks[tasks.length - 1].id + 1;
+    var taskId = tasks[tasks.length - 1].id + 1;
+
+} else {
+    console.log('hehe');
+    var tasks = [];
+    var tick = 0
+    var cross = 0
+    var taskId = 0;
+
+}
+restoreTasksBlock();
+
 
 tasksBlock.addEventListener('click', (e) => {
-    // console.log(e.target.id);
     let match = e.target.id.match(/cross(\d+)/);
     if (match) {
+        removeTask(match[1])
         document.getElementById(`task${match[1]}`).remove();
     } else {
         match = e.target.id.match(/tick(\d+)/);
         document.getElementById(`task${match[1]}`).style.backgroundColor = "#71C055";
-        console.log(match);
+        markDone(match[1])
     }
-
-
 })
 
 loginBtn.addEventListener('click', () => {
@@ -45,17 +58,22 @@ loginBtn.addEventListener('click', () => {
     }
 })
 
-
-let j = 0
 addTaskBtn.addEventListener('click', (e) => {
-    console.log('hehe');
     const task = document.createElement('div');
-    task.id = `task${taskId++}`;
-    task.innerHTML = ` <div id="taskText">${taskName.value}</div> <a class="tick" id="tick${tick++}" href="#">&#9989</a>  <a  class="cross" id="cross${cross++}" href="#">&#10060</a>`
-    task.classList.add("task")
+    saveToLocalStorage()
+    addTask(task)
+    task.classList.add("task");
     tasksBlock.appendChild(task);
 })
-
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const task = document.createElement('div');
+        saveToLocalStorage();
+        addTask(task);
+        task.classList.add("task");
+        tasksBlock.appendChild(task);
+    }
+});
 class User {
     constructor(username, password) {
         this.username = username;
@@ -64,4 +82,50 @@ class User {
     auth() {
         return user.username == "admin" && user.password == "123" ? 1 : 0
     }
+}
+
+function saveToLocalStorage() {
+    tasks.push({
+        taskName: taskName.value,
+        tick: tick++,
+        cross: cross++,
+        id: taskId++,
+        backgroundColor: "white",
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function restoreTasksBlock() {
+    console.log(tasks);
+    for (let i = 0; i < tasks.length; i++) {
+        const task = document.createElement('div');
+        task.innerHTML = ` <div id="taskText">${tasks[i].taskName}</div> <a class="tick" id="tick${tasks[i].tick}" href="#">&#9989</a>  <a  class="cross" id="cross${tasks[i].cross}" href="#">&#10060</a>`
+        task.style.backgroundColor = tasks[i].backgroundColor;
+        task.id = `task${tasks[i].id}`;
+        task.classList.add("task");
+        tasksBlock.appendChild(task);
+    }
+}
+
+function addTask(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    task.innerHTML = ` <div id="taskText">${tasks[tasks.length - 1].taskName}</div> <a class="tick" id="tick${tasks[tasks.length - 1].tick}" href="#">&#9989</a>  <a  class="cross" id="cross${tasks[tasks.length - 1].cross}" href="#">&#10060</a>`
+    task.id = `task${tasks[tasks.length - 1].id}`;
+
+}
+function removeTask(id) {
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == id) {
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function markDone(id) {
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == id) {
+            tasks[i].backgroundColor = "#71C055";
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
